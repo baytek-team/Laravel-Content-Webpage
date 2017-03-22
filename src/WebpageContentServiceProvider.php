38a864650dbee997cea471b95b2aaf8040f26303
Yvon Viger <yvon@baytek.ca>
2017-03-22 17:20:15 -0400

    Removing wildcard route for webpages

--- src/WebpageContentServiceProvider.php
+++ src/WebpageContentServiceProvider.php
@@ -73,54 +73,56 @@ class WebpageContentServiceProvider extends AuthServiceProvider
     public function bootRoutes()
     {
         // Set local namespace and make sure the route bindings occur
-        Route::group([
-                'namespace' => \Baytek\Laravel\Content\Types\Webpage::class,
-                'middleware' => ['web'],
-            ], function ($router) {
-                // Add the default route to the routes list for this provider
-                $router->resource('admin/webpage', 'WebpageController');
-                $router->get('{url}', 'WebpageController@show')->where('url', '.*?');
+        if(false) { // We should have a check if we need a wildcard route, and insert after all other routes. Must be last route.
+            Route::group([
+                    'namespace' => \Baytek\Laravel\Content\Types\Webpage::class,
+                    'middleware' => ['web'],
+                ], function ($router) {
+                    // Add the default route to the routes list for this provider
+                    $router->resource('admin/webpage', 'WebpageController');
+                    $router->get('{url}', 'WebpageController@show')->where('url', '.*?');
 
-                $router->bind('url', function ($slug) {
-                    $webpage = null;
+                    $router->bind('url', function ($slug) {
+                        $webpage = null;
 
-                    // Get the cache route id where slug
-                    if($id = collect(Cache::get('baytek.laravel.webpage.urls', []))->get($slug, false)) {
-                        $webpage = Webpage::find($id);
-                    }
-                    // Try to load the content via another method
-                    else {
-                        // Try to find the page with the slug, this should also check its parents and should also split on /
-                        $segments = collect(explode('/', $slug));
-                        $webpages = Webpage::where('contents.key', $segments->last())->ofContentType('webpage')->get();
+                        // Get the cache route id where slug
+                        if($id = collect(Cache::get('baytek.laravel.webpage.urls', []))->get($slug, false)) {
+                            $webpage = Webpage::find($id);
+                        }
+                        // Try to load the content via another method
+                        else {
+                            // Try to find the page with the slug, this should also check its parents and should also split on /
+                            $segments = collect(explode('/', $slug));
+                            $webpages = Webpage::where('contents.key', $segments->last())->ofContentType('webpage')->get();
 
-                        // Here I try to see if the url parents is the same as the url segments
-                        $webpages->each(function ($page) use ($segments, &$webpage) {
-                            // Get a list of the parents of current object
-                            $pages = collect($page->getParents());
+                            // Here I try to see if the url parents is the same as the url segments
+                            $webpages->each(function ($page) use ($segments, &$webpage) {
+                                // Get a list of the parents of current object
+                                $pages = collect($page->getParents());
 
-                            // If there is no difference of the result of the query and the URL segments we have a match
-                            if($segments->diff($pages->pluck('key'))->isEmpty()) {
-                                // Set the webpage to the match
-                                $webpage = $page;
+                                // If there is no difference of the result of the query and the URL segments we have a match
+                                if($segments->diff($pages->pluck('key'))->isEmpty()) {
+                                    // Set the webpage to the match
+                                    $webpage = $page;
 
-                                // Cache the URL
-                                $webpage->cacheUrl();
+                                    // Cache the URL
+                                    $webpage->cacheUrl();
 
-                                // Stop Processing
-                                return false;
-                            }
-                        });
-                    }
+                                    // Stop Processing
+                                    return false;
+                                }
+                            });
+                        }
 
-                    // Show the 404 page if not found
-                    if(is_null($webpage)) {
-                        abort(404);
-                    }
+                        // Show the 404 page if not found
+                        if(is_null($webpage)) {
+                            abort(404);
+                        }
 
-                    return $webpage;
+                        return $webpage;
+                    });
                 });
-            });
+        }
     }
 
 
